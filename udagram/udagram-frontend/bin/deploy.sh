171c4a4e5fbee538f1d96b7a5f3a9e5928a015f3
Osama Ahmed <osamaahmed.sus@gmail.com>
2022-07-24 23:20:05 +0200

    fix deploy script s3

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,3 @@
-aws s3 cp --recursive --acl public-read ./www s3://udagram-os/
-aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://udagram-os/
+# aws s3 cp --recursive --acl public-read ./www s3://udagram-os/
+# aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://udagram-os/
+aws s3 sync www/ s3://udagram-os
