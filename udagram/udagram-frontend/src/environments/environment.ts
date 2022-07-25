d19eb1e1e1ddc9e8b8cc035f678ce3f3a05bd06d
Osama Ahmed <osamaahmed.sus@gmail.com>
2022-07-25 12:19:32 +0200

    add api endpoint to frontend code

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -4,8 +4,9 @@
 
 export const environment = {
   production: false,
-  appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  appName: "Udagram",
+  apiHost:
+    "http://udagram-osama-env.eba-mm964rin.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
